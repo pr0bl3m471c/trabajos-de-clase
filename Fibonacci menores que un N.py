7ae174bdf1db540b5633e4a8555eb68cccd57884
pr0bl3m471c <saidamezqu@gmail.com>
2025-02-27 12:02:18 -0500

    agrego archivos trabajados en clase

---
+++ Fibonacci menores que un N.py
@@ -0,0 +1,13 @@
+n = int(input())
+penultimo = 0
+ultimo = 1
+i = 2
+print(penultimo)
+print(ultimo)
+while i < n:
+    suma = penultimo + ultimo
+    penultimo = ultimo
+    ultimo = suma
+    if ultimo < n:
+        print(ultimo)
+    i = i + 1
